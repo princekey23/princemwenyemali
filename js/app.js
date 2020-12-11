0457aae9593fd1953833159b859296330a51e5b3
princekey23 <62879759+princekey23@users.noreply.github.com>
2020-12-11 18:38:41 +0200

    minor
    just editing

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-const myButton = document.querySelector('#menu_button');
+var myButton = document.querySelector('#menu_button');
+var myLinks = document.querySelectorAll('.item_links')
 
 myButton.addEventListener('click', () =>{
     document.body.classList.toggle('nav-Open');
+});
+myLinks.addEventListener('click', () =>{
+    document.body.classList.remove('nav-Open');
 });

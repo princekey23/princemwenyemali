c29d11bb600da35aad5a8f6fe49ceeb0cde9db21
princekey23 <62879759+princekey23@users.noreply.github.com>
2020-12-11 20:26:12 +0200

    cool
    cool

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ var myLinks = document.querySelectorAll('.item_links')
 myButton.addEventListener('click', () =>{
     document.body.classList.toggle('nav-Open');
 });
-myLinks.addEventListener('click', () =>{
-    document.body.classList.remove('nav-Open');
+
+myLinks.forEach(link =>{
+	link.addEventListener('click', () =>{
+		document.body.classList.remove('nav-Open');
+	})
 });

c1ff9313c8dd225c41d31cf63bf01cd9061cbb9f
princekey23 <princemwenyemali23@gmail.com>
2021-07-28 19:09:51 +0200

    yoyo

--- js/app.js
+++ js/app.js
@@ -1,36 +1,10 @@
-var myButton = document.getElementById('search-button');
-var myLinks = document.querySelectorAll('nav-link');
+const lightbulb = document.getElementById('menu-button');
+const bluemoon = document.getElementById('menu-button2');
 
-	 // events
-
-myButton.addEventListener('click', () =>{
-    document.body.classList.toggle('nav-Open');
+lightbulb.addEventListener('click',()=>{
+  document.body.classList.add('open');
 });
 
-myLinks.forEach(link =>{
-	link.addEventListener('click', () =>{
-		document.body.classList.remove('nav-Open');
-	})
-});
-
-// back to top button
-
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+bluemoon.addEventListener('click', ()=>{
+  document.body.classList.remove('open');
+})
